b1589c0bd1b145d91552b17524e53ab6fc2e2c9f
Aaron Washburn <aaron@it-kutch.com>
2015-02-01 17:57:20 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vidorama::Application.config.secret_key_base = 'aa1a6fac3075287339e9fdc369d18e0504793fcda5581dfbf557718564cc5e29052e5ccb3fcc5fd69a3c94961e9e1bc37aeef36a357043b1ddde390e5d6a312d'
